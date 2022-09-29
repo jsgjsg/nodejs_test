19d0b36f3ef676dd0573da7fb9da4854e07a8b71
jsgjsg <jsgcys0815@gmail.com>
2022-09-29 19:40:01 +0900

    base

--- 종프/api/main.js
+++ 종프/api/main.js
@@ -1,13 +1,27 @@
 var http = require('http');
 var urlm = require('url');
 
-var app = http.createServer( function(request, response) {
-
-    console.log("github test");
-    console.log("pc github test2");
-
-    response.writeHead(200);
-    response.end('success');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'nodejs',
+    password: 'nodejs',
+    database: 'example'
 });
+db.connect();
 
+var app = http.createServer( function(request, response) {
+    var url = request.url;
+    var queryData = urlm.parse(url, true).query;
+    var pathname = urlm.parse(url, true).pathname;
+    if (request.url == '/favicon.ico') {
+        return response.writeHead(404);
+    }
+    db.query(`SELECT * FROM recipe`, function(error, recipes) {
+        console.log(recipes);
+
+        response.writeHead(200);
+        response.end('success');
+    });
+});
 app.listen(3000);
